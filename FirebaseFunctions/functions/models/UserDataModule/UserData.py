ec9afdfc74db4b29acffa9b25d61247804b32c4f
NathanWang2 <nathan.wang2@gmail.com>
2024-03-03 23:04:34 -0500

    Basic python function

---
+++ FirebaseFunctions/functions/models/UserDataModule/UserData.py
@@ -0,0 +1,15 @@
+class UserData:
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'age': self.age,
+            'email': self.email
+        }
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Email: {self.email}"
